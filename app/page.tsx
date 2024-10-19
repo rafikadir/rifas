b5570233d1ecdf466843c9ac5f5b9a2dcbc42396
Rafi Kadir <rafikadir2000@gmail.com>
2024-10-20 01:49:54 +0600

    added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FAQ from "@/components/ui/FAQ";
 import ImageCover from "@/components/ui/ImageCover";
 import ConcursosySorteos from "@/components/rifas/ConcursosySorteos";
 import { SessionWrapperWithNavBar } from "@/components/auth/SessionWrapper";
-import MainPageSkeleton from "@/components/ui/loaders/MainPageSkeleton";
+// import MainPageSkeleton from "@/components/ui/loaders/MainPageSkeleton";
 
 export default function Home() {
 	return (
